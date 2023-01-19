f3d717c090675549bdd398b8d663aa5fee5d6b95
Vladislav <111575955+glycerene@users.noreply.github.com>
2023-01-19 18:51:11 +0300

    Update UserServiceImpl.java

--- src/main/java/ru/streltsov/mymvcspringbootproject/service/UserServiceImpl.java
+++ src/main/java/ru/streltsov/mymvcspringbootproject/service/UserServiceImpl.java
@@ -21,35 +21,42 @@ public class UserServiceImpl implements UserService {
         this.userDaoImpl = userDaoImpl;
     }
 
+    @Transactional
     public UserDao getUserDaoImpl() {
         return userDaoImpl;
     }
 
+    @Transactional
     public void setUserDaoImpl(UserDao userDaoImpl) {
         this.userDaoImpl = userDaoImpl;
     }
 
     @Override
+    @Transactional
     public void addUser(User user) {
         userDaoImpl.addUser(user);
     }
 
     @Override
+    @Transactional
     public void updateUser(User user) {
         userDaoImpl.updateUser(user);
     }
 
     @Override
+    @Transactional
     public void deleteUser(int id) {
         userDaoImpl.deleteUser(id);
     }
 
     @Override
+    @Transactional
     public List<User> getAllUsers() {
         return userDaoImpl.getAllUsers();
     }
 
     @Override
+    @Transactional
     public User getUser(int id) {
         return userDaoImpl.getUser(id);
     }
